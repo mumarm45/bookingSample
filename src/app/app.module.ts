8e4996829e3a2572414bc686d65759c19406b11c
Muhammad Omar Muneer <muhammad.umar@expertflow.com>
2019-07-22 14:41:23 +0400

    push local code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,38 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-
+import { SearchComponent } from './search/search.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {ReactiveFormsModule} from '@angular/forms';
+import { ListViewComponent } from './list-view/list-view.component';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    ListViewComponent
+  ],
+  imports: [
+    BrowserModule,
+    NoopAnimationsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatListModule,
+    MatAutocompleteModule,
+    MatIconModule,
+    MatButtonModule,
+    MatGridListModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
